04098444cb3258f937a46ec17fbdb4518d42da48
TatevAgh <tatevikaghakhanyan@gmail.com>
2020-11-01 00:02:36 +0400

    third homework

---
+++ homeworks/Python homework 3/homework.py
@@ -0,0 +1,40 @@
+# Tasks related to part 4:
+# *Define a collection of weekdays, print following: first 5 week days, total days number, last 2 days, odd days
+# weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+# print('first 5 week days: ', weekdays[:5])
+# print('total days number is: ', len(weekdays))
+# print('last 2 days', weekdays[-2:])
+# print('odd days', weekdays[::2])
+
+# *Define a collection of colors, add a color from console, print total colors number, print colors on even positions
+# colors = ['blue', 'green', 'red','yellow']
+# add_color = input('Add a color: ')
+# colors.append(add_color)
+# print('total colors number: ', len(colors))
+# print('colors on even positions: ', colors[1::2])
+
+# *Define collection of numbers, get number at random position if it doesn’t exceed some inputed threshold value
+# from random import randint
+# numbers = [1, 4, 9, 77, 4, 65, 23]
+# value = int(input('input an value: '))
+# random_index = randint(0, len(numbers))
+# if numbers[random_index] < value:
+#     print('it doesn’t exceed inputed value')
+# else:
+#     print('it exceed inputed value')
+
+
+# *Define collection of students (names) from group
+# 1 and collection of students from group 2, print the collection of students from both groups
+# first_group = ['Sevak', 'Hrayr', 'Tigran', 'Tatevik']
+# second_group = ['Grigor', 'Levon', 'Arina', 'Robert']
+# print('the collection of students from both groups', first_group + second_group)
+
+
+# *Define a garage collection that stores 3 cars (models), it’s known that double of same models came to garage, print garage models
+
+
+# *Define a collection of programming languages (names), add new language name from console if it’s not C#
+# *Define an empty collection, input 3 numbers and add them to collection, calculate factorial for the last one
+# *Define a collection of words, input 2 new words and add them to list, find some inputed word in list and remove a word with some inputed index
+# *Define a game board (2x2 matrix) with 2 bomb values and 2 gold values, provide user 3 attempts to find all gold values
