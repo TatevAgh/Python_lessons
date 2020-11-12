7994cec6fae9e469e672afc3cf77194f3848d841
TatevAgh <tatevikaghakhanyan@gmail.com>
2020-11-12 16:53:41 +0400

    homework 5

---
+++ homeworks/Python homework 5/Ex_7.py
@@ -0,0 +1,9 @@
+# *Input string, find how many ‘ab’-s are inside*Input string, find how many ‘ab’-s are inside
+# str_1 = input('Input string to find ‘ab’-s count: ')
+# count = 0
+# for i in range(0, len(str_1)):
+#     if str_1[i] =='a':
+#         print(str[i]+1)
+#     # if str_1[i] == 'a':
+#     #     if str_1[i+1] =='b':
+#     #         print('yess')
