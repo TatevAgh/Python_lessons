b9b73172fc1def2c507948b25334952ec5d5d3ab
TatevAgh <tatevikaghakhanyan@gmail.com>
2020-11-17 03:36:11 +0400

    homework 6

---
+++ homeworks/Python homework 6/Ex2.py
@@ -0,0 +1,5 @@
+# *Create a collection for storing unique book names, add some of them from console and print results
+
+books = ['Harry Potter', 'The Diary Of A Young Girl', 'The Great Gatsby']
+books.append(input('Write your favorite Book name: '))
+print(books)
---
+++ homeworks/Python homework 6/Ex3.py
@@ -0,0 +1,4 @@
+# *Create 2 collections storing company employees, get number of employees working in both companies,
+# get collection of employees with some name from both of groups,
+# input several names and find if they work in one of companies, print results
+
