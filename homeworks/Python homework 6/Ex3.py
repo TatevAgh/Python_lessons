3e488f86ed437e10a3ade443dc216e423c2f91d6
TatevAgh <tatevikaghakhanyan@gmail.com>
2020-11-17 04:04:43 +0400

    homework 6

--- homeworks/Python homework 6/Ex3.py
+++ homeworks/Python homework 6/Ex3.py
@@ -2,3 +2,7 @@
 # get collection of employees with some name from both of groups,
 # input several names and find if they work in one of companies, print results
 
+first_company = ['Anny', 'Avo', 'Vars', 'Sed', 'Sona']
+second_company = ['Henry', 'Georgy', 'Anny', 'Toma']
+print('Number of employees working in both companies: ', len(first_company) + len(second_company))
+
