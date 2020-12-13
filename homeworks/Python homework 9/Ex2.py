09abaf639f38e8f466081f80436ec549bdaa1bf6
TatevAgh <tatevikaghakhanyan@gmail.com>
2020-12-13 21:06:24 +0400

    homework from lesson 9

--- homeworks/Python homework 9/Ex2.py
+++ homeworks/Python homework 9/Ex2.py
@@ -9,24 +9,30 @@ class Shop:
         self.products_and_prices = products_and_prices
         self.income = income
         self.owner = owner
+
     def add_employee(self, new_employee):
         self.employees.append(new_employee)
         return self.employees
+
     def del_employee(self, del_employee):
         self.employees.remove(del_employee)
         return self.employees
+
     def add_products(self, new_product, new_price):
         self.products_and_prices[new_product] = new_price
         return self.products_and_prices
+
     def find_prod_by_name(self, product_name):
         del self.products_and_prices[product_name]
         return self.products_and_prices
+
     def find_cheaper_than(self, cheaper_than_value):
         return dict(filter(lambda value: cheaper_than_value > value[1], self.products_and_prices.items()))
-        # print(self.products_and_prices.items())
+
     def change_owner(self, new_owner):
         self.owner = new_owner
         return self.owner
+
     def increase_income(self, increase_value):
         self.income *= increase_value
         return self.income
@@ -37,6 +43,12 @@ candy_products_and_prices = {'Sweets Candies': 5, 'Soft Drink': 3, 'Mineral Wate
 candy_income = 500
 candy_owner = 'Curtis James Jackson'
 candy_shop = Shop(candy_employees, candy_products_and_prices, candy_income, candy_owner)
+pet_employees = ['Anny', 'Mary', 'Joanna', 'Will']
+pet_products_and_prices = {'Dry Dog Food': 10, 'Dog Treats & Dog Bones': 15, 'Dog Toys': 7, 'Dog Clothing': 9, 'Special Food': 4}
+pet_income = 650
+pet_owner = 'Judy Anderson'
+pet_shop = Shop(pet_employees, pet_products_and_prices, pet_income, pet_owner)
+
 print('Adding employee: ', candy_shop.add_employee('Garegin'))
 print('Delete employee in shop', candy_shop.del_employee('John'))
 print('Adding product to our products: ', candy_shop.add_products('Sugar Free Candy', 5))
@@ -45,15 +57,10 @@ print('Product cheaper than 5 :', candy_shop.find_cheaper_than(5))
 print('New Owner of the Candy shop is: ', candy_shop.change_owner('Calvin Cordozar Broadus'))
 print('Increased income of the Candy shop is: ', candy_shop.increase_income(2))
 
-pet_employees = ['Anny', 'Mary', 'Joanna', 'Will']
-pet_products_and_prices = {'Dry Dog Food': 10, 'Dog Treats & Dog Bones': 15, 'Dog Toys': 7, 'Dog Clothing': 9, 'Special Food': 4}
-pet_income = 1000
-pet_owner = 'Judy Anderson'
-pet_shop = Shop(pet_employees, pet_products_and_prices, pet_income, pet_owner)
-print('Adding employee: ', candy_shop.add_employee('Betty'))
-# print('Delete employee in shop', candy_shop.del_employee('Joanna'))
-print('Adding product to our products: ', candy_shop.add_products('Dog Clothing', 9))
-print('Finded product by name: ', candy_shop.find_prod_by_name('Dog Toys'))
-print('Product cheaper than 5 :', candy_shop.find_cheaper_than(9))
-print('New Owner of the Candy shop is: ', candy_shop.change_owner('Henry Tomson'))
-print('Increased income of the Candy shop is: ', candy_shop.increase_income(3))
+print('Adding employee: ', pet_shop.add_employee('Betty'))
+print('Delete employee in shop', pet_shop.del_employee('Joanna'))
+print('Adding product to our products: ', pet_shop.add_products('Dog Clothing', 9))
+print('Finded product by name: ', pet_shop.find_prod_by_name('Dog Toys'))
+print('Product cheaper than 5 :', pet_shop.find_cheaper_than(9))
+print('New Owner of the Pet shop is: ', pet_shop.change_owner('Henry Tomson'))
+print('Increased income of the Pet shop is: ', pet_shop.increase_income(3))
