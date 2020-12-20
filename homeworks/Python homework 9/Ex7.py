0fdf50f7b044a6a9dffc6c0399466f7bef0f0eb2
TatevAgh <tatevikaghakhanyan@gmail.com>
2020-12-21 01:06:00 +0400

    homework from 9/10 lessons

--- homeworks/Python homework 9/Ex7.py
+++ homeworks/Python homework 9/Ex7.py
@@ -2,10 +2,8 @@
 # create a class for bear and class for wolf, create objects and use them
 
 class Animal:
-    def __init__(self, name,  habitat, sex, reproduce, features, food, animal_type):
+    def __init__(self, name,  habitat, sex, features, food, animal_type):
         self.habitat = habitat
-        self.sex = sex
-        self.reproduce = reproduce
         self.features = features
         self.food = food
         self.name = name
@@ -16,28 +14,33 @@ class Animal:
 
     def presentation_animal(self):
         print('Our animal is a', self.animal_type)
+        print('his name is :', self.name)
+        print('his features are:', self.features)
 
+    def add_features(self):
+        new_features =  input('you can write new feature for your animal: ')
+        self.features.append(new_features)
+        return self.features
 
 
 class Bear(Animal):
-    def __init__(self, name,  habitat, sex, reproduce, features, food, animal_type, color, zoo_location):
-        Animal.__init__(self, name, habitat, sex, reproduce, features, food, animal_type)
+    def __init__(self, name,  habitat, sex, features, food, animal_type, color, zoo_location):
+        Animal.__init__(self, name, habitat, sex, features, food, animal_type)
         self.color = color
         self.zoo_location = zoo_location
 
 
-
-
 class Wolf(Animal):
-    def __init__(self, name,  habitat, sex, reproduce, features, food, animal_type, color, location, position):
-        Animal.__init__(self, name, habitat, sex, reproduce, features, food, animal_type)
+    def __init__(self, name,  habitat, sex, features, food, animal_type, color, location, position):
+        Animal.__init__(self, name, habitat, sex, features, food, animal_type)
         self.color = color
         self.location = location
         self.position = position
 
-misha_bear = Bear('Misha', 'land', 'male', 'mammal', ['two legs', 'rich fur'], ['fish', 'meat', 'greens'], 'bear', 'brown', 'moscow zoo',)
-hector_wolf = Wolf('Hector', 'forest', 'male', 'mammal', ['four legs', 'predator'], ['other animals', 'meat'], 'wolf', 'gray', 'land', 'alpha',)
+misha_bear = Bear('Misha', 'land', 'male', ['two legs', 'rich fur'], ['fish', 'meat', 'greens'], 'bear', 'brown', 'moscow zoo',)
+hector_wolf = Wolf('Hector', 'forest', 'male', ['four legs', 'predator'], ['other animals', 'meat'], 'wolf', 'gray', 'land', 'alpha',)
 misha_bear.eating()
 hector_wolf.eating()
-
+print('Wolf with new features', hector_wolf.add_features())
+misha_bear.presentation_animal()
 
---
+++ homeworks/Python homework 9/Ex8.py
@@ -0,0 +1,2 @@
+# *Create a class for representing a smartphone with related attributes
+# (private, public, protected) and behavior, create objects and use them
