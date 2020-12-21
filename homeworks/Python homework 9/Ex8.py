3b91d2f9bb61e5734c8ecc00ffb83c290cf4a36a
TatevAgh <tatevikaghakhanyan@gmail.com>
2020-12-21 22:48:16 +0400

    homework from 9/10 lessons

--- homeworks/Python homework 9/Ex8.py
+++ homeworks/Python homework 9/Ex8.py
@@ -1,2 +1,22 @@
 # *Create a class for representing a smartphone with related attributes
 # (private, public, protected) and behavior, create objects and use them
+
+class Smartphone:
+    def __init__(self, model, camera, storage, color, serial_number, password):
+        self.model = model
+        self.camera = camera
+        self.storage = storage
+        self.color = color
+        self.serial_number = serial_number
+        self.password = password
+
+
+
+class Iphone(Smartphone):
+    def __init__(self, model, camera, storage, color, serial_number, password, software_version):
+        Smartphone.__init__(self, model, camera, storage, color, serial_number, password)
+        self.software_version = software_version
+
+
+
+
