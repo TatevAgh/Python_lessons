03d5dff030ae623668b2a7e858c5ba42494a9b89
TatevAgh <tatevikaghakhanyan@gmail.com>
2020-12-21 23:09:01 +0400

    some small changes

--- lesson10.py
+++ lesson10.py
@@ -1,25 +1,32 @@
 # class Animal:
+#
 #     def __init__(self, name, sex, animal_type):
 #         self.name = name
 #         self.sex = sex
-#         self.anymal_type = animal_type
+#         self.animal_type = animal_type
+#
 #     def eat(self):
 #         print(self.name, 'is eating')
 #
 # class Lion(Animal):
+#
 #     def __init__(self, name, sex, animal_type, hair_length):
 #         Animal.__init__(self,name, sex, animal_type)
 #         self.hair_length = hair_length
+#
 #     def hunt(self):
 #         print(self.name, 'hunts zafras')
+#
 #     def eat(self):
 #         super().eat()
 #         print(self.name, 'is eating a meat')
 #
 # class Elephant(Animal):
+#
 #     def __init__(self, name, sex, animal_type, hobbot_length):
 #         Animal.__init__(self, name, sex, animal_type)
 #         self.hobbot_length = hobbot_length
+#
 #     def drink(self):
 #         print(self.hobbot_length, 'this is length of elefant\s hobbot')
 #
@@ -34,39 +41,39 @@
 # super() ete uni parent class, karoxenq grel super().merUzacFunctian vercnel
 # isinstance(obj, type) stugume classe veradacnum e bool
 # getter and Setter \stacoxner ev poxoxner
-#
-# class Date:
-#     def __init__(self, day, month, year):
-#         self.day = day
-#         self.month = month
-#         self.year = year
-#     @property
-#     def day(self):
-#         return self._day
-#     @day.setter
-#     def day(self, new_day):
-#         if new_day >= 1 and new_day <= 15:
-#             self._day = new_day
-#
-#     def print_day(self):
-#         print(self.day)
-#
-#
-# my_birthday = Date(14, 4, 1996)
-# my_birthday.day = -2
-# my_birthday.print_day()
-# my_birthday.day = 10
-# my_birthday.print_day()
 
-# def divide():
-#     try:
-#         print(int(input('first num: ')) // int(input('sec num: ')))
-#     except ZeroDivisionError:
-#         print('problem with dividing with Zero')
-#     except:
-#         print('some other problem')
-#     finally:
-#         print('the action is finish')
+class Date:
+    def __init__(self, day, month, year):
+        self.day = day
+        self.month = month
+        self.year = year
+    @property
+    def day(self):
+        return self._day
+    @day.setter
+    def day(self, new_day):
+        if new_day >= 1 and new_day <= 15:
+            self._day = new_day
+
+    def print_day(self):
+        print(self.day)
+
+
+my_birthday = Date(14, 4, 1996)
+my_birthday.day = -2
+my_birthday.print_day()
+my_birthday.day = 10
+my_birthday.print_day()
+
+def divide():
+    try:
+        print(int(input('first num: ')) // int(input('sec num: ')))
+    except ZeroDivisionError:
+        print('problem with dividing with Zero')
+    except:
+        print('some other problem')
+    finally:
+        print('the action is finish')
 
 # divide()
 # print('some text after divide')
--- lesson9.py
+++ lesson9.py
@@ -4,80 +4,95 @@
 # methode funkcia e vore patkanum e konkret obyekti
 # obyekte uni bazmativ hatkutyunner, abstractione ayn e vor  anjatum e karevore)
 # encapsulation - detalnere taqcnum e ayl clasneric .......liqe kud eka grac, avel info chtal ayl obyectnerin
-#
+
 # class Person:
 #     """"this is class for Person"""
+
 #     def __init__(self, name, year):
 #         self.name = name
 #         self.year = year
 #         self.sex = 'Male'
+
 #     def printing(self):
 #         print('this is name:', self.name)
 #         print('this is year:', self.year)
 #         print('this is sex:', self.sex)
+
 #     def greet(self, last_name):
 #         print('good afternoon', last_name)
 #
 # john = Person(name = 'john', year = 1996)
 # john.printing()
 # john.greet('Berberyan')
-#
-#
-# class House:
-#     """"this is house class"""
-#     number = 0
-#     created_houses = []
-#     def __init__(self, number, floors, signalisation, is_open):
-#         self.number = number
-#         self.floors = floors
-#         self.__signalisation = signalisation
-#         self.__is_open = is_open
-#         House.number += 1
-#         House.created_houses.append(self)
-#     def change_number(self, new_number):
-#         self.number = new_number
-#     def increment_floors(self):
-#         self.floors += 1
-#     def decrement_floors(self):
-#         self.floors -= 1
-#     def close(self):
-#         self.__is_open = False
-#         self.__turn_on()
-#     def open(self):
-#         self.__is_open = True
-#         self.__turn_off()
-#     def __turn_on(self):
-#         if not(self.__signalisation):
-#             self.__signalisation = True
-#     def __turn_off(self):
-#         if self.__signalisation:
-#             self.__signalisation = False
-#     def check_is_open(self):
-#         if self.__is_open:
-#             print('the house is Open')
-#         else:
-#             print('the house is Close')
-#     def print_house_number():
-#         print('this is number of created Houses:', House.number)
-#
-# first_house = House(17, 2, False, False)
-# second_house = House(25, 1, True, True)
-# print('Number of created Houses', House.number)
-# print('Names of created Houses', House.created_houses)
-# House.print_house_number()
-#
-# first_house.increment_floors()
-# print(first_house.floors)
-# first_house.decrement_floors()
-# print(first_house.floors)
-# first_house.check_is_open()
-# second_house.check_is_open()
+
+
+class House:
+    """"this is house class"""
+    number = 0
+    created_houses = []
+
+    def __init__(self, number, floors, signalisation, is_open):
+        self.number = number
+        self.floors = floors
+        self.__signalisation = signalisation
+        self.__is_open = is_open
+        House.number += 1
+        House.created_houses.append(self)
+
+    def change_number(self, new_number):
+        self.number = new_number
+
+    def increment_floors(self):
+        self.floors += 1
+
+    def decrement_floors(self):
+        self.floors -= 1
+
+    def close(self):
+        self.__is_open = False
+        self.__turn_on()
+
+    def open(self):
+        self.__is_open = True
+        self.__turn_off()
+
+    def __turn_on(self):
+        if not(self.__signalisation):
+            self.__signalisation = True
+
+    def __turn_off(self):
+        if self.__signalisation:
+            self.__signalisation = False
+
+    def check_is_open(self):
+        if self.__is_open:
+            print('the house is Open')
+        else:
+            print('the house is Close')
+
+    def print_house_number(self):
+        print('this is number of created Houses:', House.number)
+
+first_house = House(17, 2, False, False)
+second_house = House(25, 1, True, True)
+print('Number of created Houses', House.number)
+print('Names of created Houses', House.created_houses)
+House.print_house_number()
+
+first_house.increment_floors()
+print(first_house.floors)
+first_house.decrement_floors()
+print(first_house.floors)
+first_house.check_is_open()
+second_house.check_is_open()
 
 class Celsius:
     def __init__(self, temperature = 0):
         self.set_temperature(temperature)
+
     def get_temperature(self):
         return self._temperature
+
     def set_temperature(self, value):
         if value > -273:
             self._temperature = value
